38c8ed282f5ce384d44653def78826725bf3fc20
Hyperion97 <amitc97@gmail.com>
2014-05-24 15:40:54 -0400

    create project view updated

--- projects/views.py
+++ projects/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def create_project(request):
     def render_page():
         return render(request, 'projects/create_project.html')
-
+        
     return render_page()
